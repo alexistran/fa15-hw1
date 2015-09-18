d80a199a19f34a011e8683cd9e8fd2d70d2b4df6
Alexis <alexistran@berkeley.edu>
2015-09-18 02:54:01 +0000

    message

--- foobar.rb
+++ foobar.rb
@@ -1,5 +1,10 @@
 class Foobar
-  # Q4 CODE HERE
-  def jkdfjlsajfdkl
+  def baz(a)
+  	a = a.map! {|e| e.to_i}
+  	a = a.map! {|e| e + 2}
+  	a = a.delete_if {|n| n%2 == 1}
+  	a = a.uniq
+  	a = a.delete_if {|n| n > 10}
+  	return a.inject(:+)
   end
 end
